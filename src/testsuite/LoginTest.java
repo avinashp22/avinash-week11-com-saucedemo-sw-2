4776c7d916c834ae06ddcaf0a1fbf100e27377bf
avinashp22 <avinashp22@gmail.com>
2023-11-18 20:19:29 +0000

    avinash-week11-com-saucedemo-sw-2

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -1,4 +1,34 @@
 package testsuite;
 
-public class LoginTest {
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginTest extends BaseTest {
+
+    String baseURL = "https://www.saucedemo.com/"; // set base url
+
+    @Before
+    public void setUP(){openBrowser(baseURL);}
+
+    @Test
+    public void nameHere1(){
+
+
+    }
+
+
+
+    @Test
+    public void nameHere2(){
+
+
+
+    }
+
+    @After
+    public void tearDown(){closeBrowser();}
+
+
 }
