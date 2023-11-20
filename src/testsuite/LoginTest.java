3e8b3abff48f834613b51992fec91f0bbd0658ae
avinashp22 <avinashp22@gmail.com>
2023-11-20 13:35:58 +0000

    avinash-week11-com-saucedemo-sw-2

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -2,30 +2,30 @@ package testsuite;
 
 import browserfactory.BaseTest;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class LoginTest extends BaseTest {
 
     String baseURL = "https://www.saucedemo.com/"; // set base url
 
     @Before
-    public void setUP(){openBrowser(baseURL);}
+    public void setUp(){openBrowser(baseURL);}
 
     @Test
-    public void nameHere1(){
+    public void userShouldLoginSuccessfullyWithValidCredentials(){
 
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String expected = "Products";
+        String actual = driver.findElement(By.xpath("//span[text() = 'Products']")).getText();
+        Assert.assertEquals("Correct Message not Displayed",expected,actual);
 
     }
 
-
-
-    @Test
-    public void nameHere2(){
-
-
-
-    }
 
     @After
     public void tearDown(){closeBrowser();}
